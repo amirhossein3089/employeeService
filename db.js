534d7b0083bc8fa78370c02338a93b5031cf37b7
Amirhossein <amirhossein3089@yahoo.com>
2019-06-19 00:08:14 +0430

    changed for exporting db

--- db.js
+++ db.js
@@ -1,11 +1,29 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
-const connectionURL = 'mongodb://127.0.0.1:27017'
-const databaseName = 'employeeService'
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client)=> {
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+const connectionURL = "mongodb://127.0.0.1:27017";
+const databaseName = "employeeService";
+
+let callback, db;
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true },
+  (error, client) => {
     if (error) {
-        return console.log('Unable to connect to database!')
+      return console.log("Unable to connect to database!");
     }
-    const db = client.db(databaseName)
+    const db = client.db(databaseName);
     // Start to interact with the database
-})
+
+    db = client.db(dbName);
+    callback(db);
+  }
+);
+
+module.exports = function(cb) {
+  if (typeof db != "undefined") {
+    cb(db);
+  } else {
+    callback = cb;
+  }
+};
--- service.js
+++ service.js
@@ -16,29 +16,33 @@ exports.sampleRequest = function(req, res) {
   res.end(JSON.stringify(response));
 };
 
-exports.testRequest = function (req, res) {
-    body = '';
+exports.testRequest = function(req, res) {
+  body = "";
 
-    req.on('data', function (chunk) {
-        body += chunk;
-    });
+  req.on("data", function(chunk) {
+    body += chunk;
+  });
 
-    req.on('end', function () {
+  req.on("end", function() {
+    postBody = JSON.parse(body);
 
-        postBody = JSON.parse(body);
+    var response = {
+      text: "Post Request Value is  " + postBody.value
+    };
 
-        var response = {
-            "text": "Post Request Value is  " + postBody.value
-        };
+    // const mongo = require("./db");
+    // mongo(db => {
+    //   //here get db handler
+    // });
 
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(response));
-    });
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(response));
+  });
 };
 
-exports.invalidRequest = function (req, res) {
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Invalid Request');
+exports.invalidRequest = function(req, res) {
+  res.statusCode = 404;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Invalid Request");
 };
